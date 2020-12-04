99b31d72bdaeaa8a99013ed20a3f5435566336ac
Mitchell A Moore <mitchell1484@gmail.com>
2020-12-04 16:14:51 -0600

    Save progress

--- parse_data.py
+++ parse_data.py
@@ -14,9 +14,32 @@ def parse_month(directory, year, month):
     pass
 
 
-def parse_day(directory, year, month, day):
+def parse_day(directory, date):
+    """
+    :param directory: string directory where data files lies
+    :param date: string of format 'YYYY-MM-DD'
+    :return: requested data as json
+    """
+
+    suffix = ".txt"
+    data_output = {"date": date, "time": list(), "temp": list(),
+                   "intensity": list(), "red": list(), "green": list(), "blue": list()}
+    with open((directory + date + suffix), "r") as f:
+        f.readline()
+        count = 0
+        for line in f.readlines():
+            print(line)
+            last_line = f.readline().replace('\n', '')
+            last_line = last_line.split("\t")
+        # todo: pick up from this point
+            data_output["time"].append(last_line[1])
+            count = count + 1
+            if count == 20:
+                break
+        f.close()
     pass
 
+parse_day(vars.data_dir, "2020-04-01")
 
 def parse_recent(directory):
     suffix = ".txt"
